8d62ccf0563b428926ffaa4aa666a3ea98d1c25a
y12uc231 <krishna.satyapriya123@gmail.com>
2015-04-06 07:02:12 +0530

    mod sample inserted

---
+++ mod1.py
@@ -0,0 +1,10 @@
+import sys
+import platform
+print sys.argv
+print sys.byteorder
+print sys.builtin_module_names
+print sys.copyright
+ 
+ #platform
+print platform.machine()  #Microprocessor
+print platform.node()
