4c94a55c01ebf122b63754a84b09407f8bd1bdb1
Satyapriya Krishna <krishna.satyapriya123@gmail.com>
2015-04-11 10:18:01 +0530

    Create tuple.py

---
+++ tuple.py
@@ -0,0 +1,8 @@
+#This file is an introduction to tuples in python
+#Tuples are similar to lists , but, tuples are more strict than lists.
+#Tuples are immutable
+
+t=(2,3,4) #Tuple Declaration
+t[2]=45 #Trackback
+#All the functions applicable to  tuples can be extracted from dir() command
+
